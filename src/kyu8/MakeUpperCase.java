a8956bb6a1d115daf9a2417dd37d5ad472baba1c
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-03-18 22:45:26 +0300

    add task: 'MakeUpperCase'

---
+++ src/kyu8/MakeUpperCase.java
@@ -0,0 +1,10 @@
+package kyu8;
+
+/**
+ * Напишите функцию, которая преобразует входную строку в верхний регистр.
+ */
+public class MakeUpperCase {
+    public static String MakeUppercase(String str) {
+        return str.toUpperCase();
+    }
+}
