1861befbaf1117a341de6bf4d2e8ba58acadab71
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-03-24 03:28:01 +0300

    add task: 'Convert a string to an array'

---
+++ src/kyu8/ConvertStringToAnArray.java
@@ -0,0 +1,14 @@
+package kyu8;
+
+import java.util.Arrays;
+
+/**
+ * Преобразовать строку в массив
+ * 
+ * Напишите функцию для разделения строки и преобразования ее в массив слов.
+ */
+public class ConvertStringToAnArray {
+    public static String[] stringToArray(String s) {
+        return s.split("\s");
+    }
+}
