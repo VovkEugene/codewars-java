18b5ca9e80e248a07a7aed7391a0a1adfec48e21
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-03-26 21:45:03 +0300

    add task: 'Exes and Ohs'

---
+++ src/kyu7/ExesAndOhs.java
@@ -0,0 +1,43 @@
+package kyu7;
+
+import java.util.HashMap;
+
+/**
+ * Проверьте, содержит ли строка одинаковое количество символов "x" и "o".
+ * Метод должен возвращать логическое значение и не учитывать регистр.
+ * Строка может содержать любой символ.
+ */
+public class ExesAndOhs {
+//    public static boolean getXO(String str) {
+//        int charX = 0;
+//        int charO = 0;
+//
+//        char[] chars = str.toLowerCase().toCharArray();
+//
+//        for (char ch : chars) {
+//            if (ch == 'x') {
+//                charX++;
+//            }
+//            if (ch == 'o') {
+//                charO++;
+//            }
+//        }
+//
+//        return charX == charO;
+//    }
+
+    public static boolean getXO(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        char[] chars = str.toLowerCase().toCharArray();
+
+        for (char ch : chars) {
+            if (ch == 'x' || ch == 'o') {
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+            }
+        }
+
+        return map.getOrDefault('x', 0)
+                .equals(map.getOrDefault('o', 0));
+    }
+}
